d51a625f56a09aab6f6d7600dd9c35107aac54b1
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-07-17 10:35:36 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+"""
+Just a simple hello-world app.
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run()
